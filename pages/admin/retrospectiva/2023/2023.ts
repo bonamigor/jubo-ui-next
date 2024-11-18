9c7056fd9b5d84838f6c20ad7e599a3c900d40f7
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2024-11-18 19:30:15 -0300

    Estrutura inicial da retrospectiva

---
+++ pages/admin/retrospectiva/2023/2023.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  color: hsl(228, 12%, 48%);
+`
+
+export const Section = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: hsl(30, 38%, 92%);
+`
+
---
+++ pages/admin/retrospectiva/2023/index.page.tsx
@@ -0,0 +1,36 @@
+import { NextPage } from "next"
+import { Container, Section } from "./2023"
+
+
+const VinteTres: NextPage = () => {
+  return (
+    <Container>
+      <Section>
+        Inicial
+      </Section>
+      <Section>
+        Primeiro
+      </Section>
+      <Section>
+        Segundo
+      </Section>
+      <Section>
+        Terceiro
+      </Section>
+      <Section>
+        Quarto
+      </Section>
+      <Section>
+        Quinto
+      </Section>
+      <Section>
+        Sexto
+      </Section>
+      <Section>
+        Finalização
+      </Section>
+    </Container>
+  )
+}
+
+export default VinteTres
